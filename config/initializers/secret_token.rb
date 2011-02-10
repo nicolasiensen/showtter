1ab06b1142faaa85e0e089460bec97067f0a3c7f
Nícolas Iensen <nicolas.iensen@gmail.com>
2011-02-10 04:30:26 -0200

    First commit, project template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtter::Application.config.secret_token = '798ed47ae431e11b6e2c9682be4a0aba3a288e0540b09e350a57e7010674ff7d719b2cc0fadc5f1f73d9eb6664af19cbf34b1ec251f07358ef0fdb23abc5ce87'
